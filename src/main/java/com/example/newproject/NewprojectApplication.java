77c1a109870dd96e043352b06bcb08690cd3b0bd
YouKnowKim <h3969@naver.com>
2021-12-21 16:29:38 +0900

    Modify Project

--- src/main/java/com/example/newproject/NewprojectApplication.java
+++ src/main/java/com/example/newproject/NewprojectApplication.java
@@ -2,13 +2,19 @@ package com.example.newproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NewprojectApplication {
+public class NewprojectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(NewprojectApplication.class, args);
-	}
+        SpringApplication.run(NewprojectApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NewprojectApplication.class);
+    }
 }
