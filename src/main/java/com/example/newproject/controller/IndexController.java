f8f0388162debffd4c17a23f57e7f42724f2f14a
YouKnowKim <h3969@naver.com>
2021-12-21 16:04:15 +0900

    Add IndexController

--- src/main/java/com/example/newproject/controller/IndexController.java
+++ src/main/java/com/example/newproject/controller/IndexController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    @GetMapping("/hello")
-    public String hello() {
+    @GetMapping("/hellos")
+    public String hell() {
         return "Hell";
     }
 }
